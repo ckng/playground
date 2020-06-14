56fea7bc93c21dd0793a0e2e3bebb6fabd07c38a
Ng Chin Kiong <ckng@endlessthemes.com>
2020-06-14 17:06:56 +0800

    restructured; default template

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,15 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: ['./public/**/*.html'],
+  content: [
+    './public/**/*.html'
+  ],
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
 })
 
 module.exports = {
   plugins: [
+    require('postcss-easy-import'),
+    require('postcss-import'),
+    require('postcss-nested'),
     require('tailwindcss'),
     require('autoprefixer'),
     ...process.env.NODE_ENV === 'production'
